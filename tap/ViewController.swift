2b0c80555efd6740e5f48903238a34b0c84b881d
20171105145 <20171105145du@admindeiMac-62.local>
2018-12-06 10:04:26 +0800

    Littele game
    未完成警告框，其他已结束，下节课背景及警告框，需修改加号

--- tap/ViewController.swift
+++ tap/ViewController.swift
@@ -11,11 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     var num = 0 //num用于计算tap me 按键数
     var time = 0//time用于追踪秒数
-    
+
     var  overtime = Timer()//overtime用timer 描述时间是否终止
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     @IBOutlet weak var timerL: UILabel!
     @IBOutlet weak var scoreL: UILabel!
@@ -26,15 +25,20 @@ class ViewController: UIViewController {
             setupGame()
             subtracTimer()
         }
-        num = num + 1
-        scoreL.text = "Score:\(num)"
+        
+        if time > 0{
+            num = num + 1
+            scoreL.text = "score\(num)"
+        }
+        else { scoreL.text = "时间已结束，score\(num)"
+        }
         
     }
     func setupGame(){
         time = 30
         timerL.text = "Time:\(time)"
-        scoreL.text = "score\(num)"
-         overtime = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(subtracTimer), userInfo: nil, repeats: true)//
+        scoreL.text = "score:\(num)"
+        overtime = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(subtracTimer), userInfo: nil, repeats: true)//
         
     }
     @objc func subtracTimer(){
@@ -45,8 +49,14 @@ class ViewController: UIViewController {
             overtime.invalidate()//阻止时间减少
         }
 }
-
-
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let alert = UIAlertController(title: "Time  Is Up!", message: "You scored/(count) points", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Play Again", style: UIAlertAction.Style.default, handler: {
+            action in self.setupGame()
+        }),
+self.presentedViewController(alert,animated: true,completion: nil)
+        )
+        
+    }
 }
